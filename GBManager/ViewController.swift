641981a5feac174edbb3912d0c93107b450d40ee
walker彬 <walker@guobindeMacBook-Pro.local>
2017-04-21 14:43:23 +0800

    first initial

---
+++ GBManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GBManager
+//
+//  Created by walker彬 on 2017/4/21.
+//  Copyright © 2017年 walker彬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
