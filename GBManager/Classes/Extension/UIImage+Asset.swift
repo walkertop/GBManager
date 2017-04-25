0a83f228ba47f2fe3c1cc20ebd6dbd34c2bc8490
walker彬 <bin5211bin@gmail.com>
2017-04-25 15:45:30 +0800

    添加完成了tabbarController

---
+++ GBManager/Classes/Extension/UIImage+Asset.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+Asset.swift
+//  GBManager
+//
+//  Created by walker彬 on 2017/4/25.
+//  Copyright © 2017年 walker彬. All rights reserved.
+//
+
+import Foundation
---
+++ GBManager/Classes/Extension/UIViewController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Extension.swift
+//  GBManager
+//
+//  Created by walker彬 on 2017/4/25.
+//  Copyright © 2017年 walker彬. All rights reserved.
+//
+
+import Foundation
