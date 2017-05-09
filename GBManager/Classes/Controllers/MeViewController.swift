43f0475fa84029fcc82dc771b735f10abd41a434
walker彬 <bin5211bin@gmail.com>
2017-05-09 15:03:32 +0800

    Add 我的界面

--- GBManager/Classes/Controllers/MeViewController.swift
+++ GBManager/Classes/Controllers/MeViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class MeViewController: UIViewController {
-
+class MeViewController: UIViewController{
+    
+    fileprivate let itemDataSource : [[(name: String, iconImage: UIImage?)]] = [
+        [("",nil)],             //头像，第一组
+        [("相册",nil),           //第二组
+         ("收藏",nil),
+         ("钱包",nil),
+         ("卡包",nil)
+        ],
+        [("表情",nil)],           //第三组
+        [("设置",nil)]]           //第四组
+    
+    var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        setupTableView()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -33,4 +46,66 @@ class MeViewController: UIViewController {
     }
     */
 
+    func setupTableView() {
+        tableView = UITableView(frame: UIScreen.main.bounds)
+        tableView?.delegate = self as? UITableViewDelegate
+        tableView?.dataSource = self as? UITableViewDataSource
+        
+        tableView?.register(UINib.init(nibName: "MeAvatarTableViewCell", bundle: nil), forCellReuseIdentifier: "MeAvatarTableViewCell")
+        view.addSubview(tableView!)
+    }
+    
+
+}
+
+//MARK: Delegate
+
+extension MeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 15
+        } else {
+            return 20
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       
+        if indexPath.section == 0 {
+            return 88
+        } else {
+            return 44
+        }
+    }
+}
+
+//MARK: @protocol - UITableViewDataSource
+extension MeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return itemDataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let rows = itemDataSource[section]
+        return rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell: MeAvatarTableViewCell = tableView.dequeueReusableCell(withIdentifier: "MeAvatarTableViewCell", for: indexPath) as! MeAvatarTableViewCell
+            return cell
+
+        } else {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            cell.accessoryType = .disclosureIndicator
+            cell.textLabel?.text = itemDataSource[indexPath.section][indexPath.row].name
+            cell.imageView?.image = itemDataSource[indexPath.section][indexPath.row].iconImage
+            return cell
+        }
+    }
+    
 }
